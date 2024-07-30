7c20495b1fc8d19800be6950ab3b7daad6c8db85
Kmina <segmentree@gmail.com>
2024-07-30 11:30:35 +0200

    updating hook to use tools

--- src/components/chat/chat-hub.hook.ts
+++ src/components/chat/chat-hub.hook.ts
@@ -1,6 +1,7 @@
 import { ref, Ref } from 'vue';
 import { openai } from '@ai-sdk/openai';
-import { streamText, CoreMessage } from 'ai';
+import { streamText, CoreMessage, tool } from 'ai';
+import { useRemindersTools } from '../../tools/chat-functions';
 
 export function useChatHub(
   system = '',
@@ -11,21 +12,43 @@ export function useChatHub(
   const messages = backgroundMessages;
   const conversation: Ref<string[]> = ref([]);
 
+  const { setReminder, listReminders } = useRemindersTools();
+
   async function askQuestion() {
     messages.push({ role: 'user', content: userQuestion.value });
-    conversation.value.push('');
+
     const result = await streamText({
       model: openai(model),
       system: system,
       messages,
+      tools: {
+        setReminder: tool({
+          description: setReminder.description,
+          parameters: setReminder.parameters,
+          execute: async (args) => {
+            setReminder.execute(askQuestion, userQuestion, args);
+          },
+        }),
+        listReminders: tool({
+          description: listReminders.description,
+          parameters: listReminders.parameters,
+          execute: async (args) => {
+            listReminders.execute(askQuestion, userQuestion, args);
+          },
+        }),
+      },
     });
 
-    for await (const chunk of result.textStream) {
-      conversation.value[conversation.value.length - 1] += chunk;
+    const hasNext = await result.textStream[Symbol.asyncIterator]().next();
+    if (hasNext.value) {
+      conversation.value.push('');
+      for await (const chunk of result.textStream) {
+        conversation.value[conversation.value.length - 1] += chunk;
+      }
     }
 
     result.text.then((text) => {
-      messages.push({ role: 'assistant', content: text });
+      if (text) messages.push({ role: 'assistant', content: text });
     });
 
     userQuestion.value = '';
