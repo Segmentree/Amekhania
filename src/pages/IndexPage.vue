b9d7e47f44020196aae438f7b817f530b8e7f0a2
Kmina <segmentree@gmail.com>
2024-07-31 20:07:40 +0200

    passing set of tools

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,13 @@
 <template>
   <q-page class="row q-pa-md">
-    <chat-hub />
+    <chat-hub :tools="tools" />
   </q-page>
 </template>
 
 <script setup>
 import ChatHub from 'src/components/chat/ChatHub.vue';
-defineOptions({
-  name: 'IndexPage',
-});
+import { useRemindersTools } from '../tools/chat-functions';
+
+const { setReminder, listReminders, askReminderDate } = useRemindersTools();
+const tools = [setReminder, listReminders, askReminderDate];
 </script>
