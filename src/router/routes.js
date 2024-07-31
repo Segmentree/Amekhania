45377cd2badb5de68985e29554aa25a8f7144d84
Kmina <segmentree@gmail.com>
2024-08-01 00:29:02 +0200

    routing notes and reminders

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,30 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        path: '/reminders-list',
+        name: 'RemindersList',
+        component: () => import('pages/RemindersList.vue'),
+      },
+      {
+        path: '/notes-list',
+        name: 'NotesList',
+        component: () => import('pages/NotesList.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
 
-export default routes
+export default routes;
