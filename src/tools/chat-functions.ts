1627c4cca3336077e3e5cce3fe37026b793d5057
Kmina <segmentree@gmail.com>
2024-08-02 01:10:57 +0200

    adding notes tools

--- src/tools/chat-functions.ts
+++ src/tools/chat-functions.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 import { Tool } from '../models/tool';
 import { Reminder } from '../models/reminder';
+import { Note } from '../models/note';
 import { useRemindersStore } from 'src/stores/reminders-store';
+import { useNotesStore } from 'src/stores/notes-store';
 import { proxyUnwrap } from './helpers';
 
 export function useRemindersTools() {
@@ -49,4 +51,46 @@ export function useRemindersTools() {
   };
 
   return { setReminder, listReminders, askReminderDate };
+}
+
+export function useNotesTools() {
+  const notesStore = useNotesStore();
+
+  const addNote: Tool = {
+    name: 'addNote',
+    description:
+      'Every time the user wants to take note of something or make a record of something, this tool is used.',
+    parameters: z.object({
+      title: z
+        .string()
+        .describe('Generate a title for the note based on the context'),
+      summary: z
+        .string()
+        .describe(
+          'A summary of the recent conversation or the goal of the note'
+        ),
+      date: z.string().describe('The current date in the format YYYY-MM-DD'),
+    }),
+    execute: ({ title, summary, date }) => {
+      notesStore.add({ title, summary, date });
+      return `Notify the user that the note was created with the title ${title} and the summary ${summary}. Don't say more than that.`;
+    },
+  };
+
+  const listNotes: Tool = {
+    name: 'listNotes',
+    description: 'You are asked to list all the notes that the user has taken',
+    parameters: z.object({}),
+    execute: () => {
+      const list = proxyUnwrap(notesStore.list)
+        .map(
+          (note: Note) =>
+            `{ 'title':${note.title}, 'summary':${note.summary}, 'date':${note.date}}`
+        )
+        .join(', ');
+      return `Show the user this list of notes: ${list}`;
+    },
+  };
+
+  return { addNote, listNotes };
 }
