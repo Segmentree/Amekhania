29c7bdced2c0a085ee95b6497ec6a827f4b6f261
Kmina <segmentree@gmail.com>
2024-08-02 01:06:16 +0200

    refactoring

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-item clickable :to="{ name: props.link }">
-    <q-item-section v-if="props.icon" avatar>
-      <q-icon :name="props.icon" />
+  <q-item clickable :to="{ name: link }">
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="icon" />
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>{{ props.title }}</q-item-label>
-      <q-item-label caption>{{ props.caption }}</q-item-label>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>{{ caption }}</q-item-label>
     </q-item-section>
   </q-item>
 </template>
@@ -16,7 +16,7 @@ defineOptions({
   name: 'EssentialLink',
 });
 
-const props = defineProps({
+defineProps({
   title: {
     type: String,
     required: true,
