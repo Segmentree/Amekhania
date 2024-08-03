56a664655ffb3fdccab2d1ddbf8f7c42699a115b
Kmina <segmentree@gmail.com>
2024-08-03 19:32:24 +0200

    adding new tools models

--- src/models/tool.ts
+++ src/models/tool.ts
@@ -14,3 +14,16 @@ export interface Tool {
   execute?: (args: inferParameters<Parameters>) => void;
   name: string;
 }
+
+export interface CustomParameter {
+  name: string;
+  description: string;
+  value?: string;
+}
+
+export interface CustomTool {
+  description: string;
+  parameters: CustomParameter[];
+  body: string;
+  name: string;
+}
