ae210d852db42c6d12d1b4d8715eb7cb0a2d9448
Kmina <segmentree@gmail.com>
2024-08-03 19:35:17 +0200

    using custom tools

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,8 +5,18 @@
 <script setup>
 import ChatHub from 'src/components/chat/ChatHub.vue';
 import { useRemindersTools, useNotesTools } from '../tools/chat-functions';
+import { useToolsStore } from '../stores/tools-store';
+
+const { list: customToolsList } = useToolsStore();
 
 const { setReminder, listReminders, askReminderDate } = useRemindersTools();
 const { addNote, listNotes } = useNotesTools();
-const tools = [setReminder, listReminders, askReminderDate, addNote, listNotes];
+const tools = [
+  setReminder,
+  listReminders,
+  askReminderDate,
+  addNote,
+  listNotes,
+  ...customToolsList,
+];
 </script>
